e77f2b34fe0071087ef5d4056c7defbc202aa7f6
João Pedro Queiroz <joao21731@gmail.com>
2021-09-23 14:19:18 -0300

    ✨ Adiciona novas funcionalidades e rotas

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/home';
+import Login from './pages/login';
+import SignUp from './pages/sign';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={SignUp} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
