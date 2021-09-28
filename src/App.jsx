5bd2124b765afd92480b6fae7cf765a8bf2bcfd0
Joao Rodrigues <joao21731@gmail.com>
2021-09-28 19:24:13 -0300

    🐛 Corrije bug em nome de arquivo e no yarn

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/home';
 import Login from './pages/login';
-import SignUp from './pages/sign';
+import SignUp from './pages/signup';
 
 function App() {
   return (
