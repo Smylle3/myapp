da9dad589720330c9531537e94aa67a691613f28
Smylle3 <joao21731@gmail.com>
2021-10-07 23:57:26 -0300

    💄 Estiliza página de recuperação de senha

---
+++ src/pages/forgot_pass/styles.js
@@ -0,0 +1,66 @@
+import styled, { keyframes } from 'styled-components';
+
+export const App = styled.div`
+    text-align: center;
+`;
+
+export const AppBody = styled.header`
+    background-color: #61dafb;
+    min-height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const BoxLogin = styled.div`
+    background-color: #282c34;
+    height: 40vh;
+    width: 30vw;
+
+    border-radius: 25px;
+    box-shadow: 5px 5px 20px #3e4451;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const BoxButton = styled.div`
+    width: 25vw;
+`;
+
+export const BoxLogo = styled.div`
+    height: 20vmin;
+    width: 20vmin;
+
+    border-radius: 50vmin;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    transition: 0.5s;
+    background-color: #3e4451;
+    box-shadow: 5px 5px 20px #3e4451;
+    margin-bottom: 30px;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+export const BoxLogoImg = styled.img`
+    height: 15vmin;
+    animation: ${rotate} infinite 20ms linear;
+`;
